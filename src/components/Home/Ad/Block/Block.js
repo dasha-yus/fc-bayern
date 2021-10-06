b22d01d4321428a7d9a62b2579effd7cf12a3be4
Darya Yusipets <DYusipets@gomel.iba.by>
2021-10-06 17:59:28 +0300

    temporarily removed the asynchronous request

--- src/components/Home/Ad/Block/Block.js
+++ src/components/Home/Ad/Block/Block.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import styles from './styles.module.scss';
-import { Link } from 'react-router-dom';
 
 const Block = (props) => {
 	const { img, text, link="#" } = props;
 
 	return (
-		<Link to={link} className={styles.block}>
+		<a href={link} className={styles.block}>
 			<img src={img} />
 			<p>{text}</p>
-		</Link>
+		</a>
 	);
 };
 
--- src/components/Home/Games/Games.js
+++ src/components/Home/Games/Games.js
@@ -4,18 +4,44 @@ import { Link } from 'react-router-dom';
 import styles from './styles.module.scss';
 
 const Games = () => {
-	const [game, setGame] = useState();
+	// const [game, setGame] = useState();
+	const [game, setGame] = useState({
+		group: {
+			group_name: 'Bundesliga',
+		},
+		round: {
+			name: 32,
+		},
+		match_start: '2021-05-08 16:30:00',
+		venue: {
+			name: 'Allianz Arena',
+			city: 'Munich',
+		},
+		home_team: {
+			name: 'FC Bayern Munich',
+			logo: 'https://cdn.sportdataapi.com/images/soccer/teams/100/111.png',
+		},
+		away_team: {
+			name: 'Borussia Monchengladbach',
+			logo: 'https://cdn.sportdataapi.com/images/soccer/teams/100/2815.png',
+		},
+		stats: {
+			ft_score: '6-0',
+			ht_score: '4-0',
+		},
+	});
+	// const API_KEY = '80802f90-26b3-11ec-9115-b930f3b28b29';
 
-	useEffect(() => {
-		axios
-			.get(
-				'https://app.sportdataapi.com/api/v1/soccer/matches?apikey=62dd2410-25c4-11ec-b658-91d040becc47&season_id=496&date_from=2021-04-19'
-			)
-			.then((res) => {
-				setGame(res.data.data[25]);
-			})
-			.catch((err) => console.error(err));
-	}, [game]);
+	// useEffect(() => {
+	// 	axios
+	// 		.get(
+	// 			`https://app.sportdataapi.com/api/v1/soccer/matches?apikey=${API_KEY}&season_id=496&date_from=2021-04-19`
+	// 		)
+	// 		.then((res) => {
+	// 			setGame(res.data.data[25]);
+	// 		})
+	// 		.catch((err) => console.error(err));
+	// }, [game])
 
 	return (
 		<div className={styles.games}>
